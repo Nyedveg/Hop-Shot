04eb5030fcb28c20b589421dca7c94ac7204e8f3
Nyedveg <niedzviegas@nyestudios.com>
2024-03-09 17:24:51 +0200

    reworked mechanics

---
+++ weapon_pickup.gd
@@ -0,0 +1,13 @@
+extends RigidBody3D
+
+var dropped = false
+
+func _ready():
+	pass
+	
+func _process(delta):
+	if dropped == true:
+		apply_impulse(transform.basis.z, -transform.basis.z * 10)
+		dropped = false
+		print("drop true")
+	
