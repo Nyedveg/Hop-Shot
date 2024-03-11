7c86b2ebdc08776e44a352b94ab69ad275140a6b
mentalfestas <mentalfestas@duck.com>
2024-03-11 19:11:54 +0200

    added weapon floating animation with text above it relative position

--- scripts/Action_Listener_DEMO.gd
+++ scripts/Action_Listener_DEMO.gd
@@ -6,12 +6,24 @@ var pressedD = false
 var all_pressed = false
 @onready var text_pop = $"../TextPop"
 @onready var spawn_in = $"../spawn_weapon"
+@onready var text_animation = $"../TextPop/AnimationPlayer"
 var weapons = preload("res://weapons/weapon.tscn")
+var weapon_spawn_position = Vector3(0,0.8,-5)
 
 func spawn_weapon():
 	var weapon = weapons.instantiate()
-	weapon.position = spawn_in.position
+	weapon.position = weapon_spawn_position
 	spawn_in.add_child(weapon)
+		# Calculate the position for text_pop
+	var text_offset = weapon_spawn_position  # Adjust this offset as needed
+	text_pop.global_transform.origin = weapon_spawn_position #+ Vector3(0,0.7,0)
+	
+	
+	
+	# Add text_pop as a child of spawn_in to make it relative to it
+	spawn_in.add_child(text_pop)
+	
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,6 +32,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	if Input.is_action_just_pressed("move_backward"):
 		pressedS = true
 		print("S")
@@ -35,7 +48,11 @@ func _process(delta):
 		
 	if pressedS&&pressedW&&pressedD&&pressedA&&!all_pressed:
 		all_pressed = true
+		text_pop.visible = true
+		text_animation.play("text_float")
 		spawn_weapon()
+		print("spawn_in position:", text_pop.position)
+		
 		
 		
 		
