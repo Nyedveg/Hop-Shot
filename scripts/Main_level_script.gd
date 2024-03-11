e9f6cd123da8b9e75da11881759e328dabd677c0
Nyedveg <niedzviegas@nyestudios.com>
2024-03-11 19:12:31 +0200

    Added equipinng mechanic and bullet counter

--- scripts/Main_level_script.gd
+++ scripts/Main_level_script.gd
@@ -3,22 +3,33 @@ extends Node3D
 var collider_obj = preload( "res://prefabs/collider.tscn")
 @onready var speed_label = $SpeedLabel
 @onready var player = $Player
+@onready var ammo_label = $AmmoLabel
+
+@export var startAmmo: int
+signal set_ammo(setAmmo)
+signal change_ammo(ammoCount)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	emit_signal("set_ammo", startAmmo)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	speed_label.text = var_to_str(player.velocity)
-	pass
 
 # A signal emmited by the player, when the weapon gets shot
 func _on_player_player_shot_fired(pos):
 	var collider = collider_obj.instantiate()
 	add_child(collider)
 	collider.position = pos
+	startAmmo -= 1
 
 func _on_death_zone_body_entered(body):
 	print("You died")
 	body.position = Vector3.ZERO + Vector3(0,5,0)
+
+func _on_ammo_create_change_ammo(ammoCount):
+	emit_signal("change_ammo", ammoCount)
+
+func _on_player_player_update_ammo(currentAmmo):
+	ammo_label.text = var_to_str(currentAmmo)
