e2bf4150a38717cb8e99345605345c9a394e6336
Nyedveg <niedzviegas@nyestudios.com>
2024-03-11 17:43:20 +0200

    Fixed borken player raycast

--- prefabs/player/Weapon_handler_script.gd
+++ prefabs/player/Weapon_handler_script.gd
@@ -10,7 +10,6 @@ signal shot_fired(pos)
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	#WEAPON SHOOTING
@@ -20,4 +19,3 @@ func _physics_process(_delta):
 			if aimcast.is_colliding() && aimcast.get_collider().is_in_group("shootable"):
 				emit_signal("shot_fired", aimcast.get_collision_point())
 				var projectile_vector = position - aimcast.get_collision_point()
-				player.velocity += projectile_vector / 10
