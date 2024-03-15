2211e449bd54bb9ed97f48bf750aba25788889b2
Nyedveg <niedzviegas@nyestudios.com>
2024-03-15 12:30:54 +0200

    Tweaked FOV settings to make smoother camera transitions

--- scripts/UI_main_script.gd
+++ scripts/UI_main_script.gd
@@ -4,14 +4,16 @@ extends Control
 @onready var speed_label = $SpeedLabel
 @onready var level = $".."
 @onready var player = $"../Player"
+@onready var fov = $FOV
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ammo_label.text = var_to_str(level.startAmmo)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	speed_label.text = var_to_str(ceil(player.velocity.length()))
+	fov.text = var_to_str(player.camera3d.fov)
 
 func _on_player_ui_update_ammo(currentAmmo):
 	ammo_label.text = var_to_str(currentAmmo)
